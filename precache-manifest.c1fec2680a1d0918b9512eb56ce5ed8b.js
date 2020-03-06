19730f4f04ebfee66b01d3e98f5bb0deb0c2c11e
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-03-06 19:18:01 +0300

    Updates

---
+++ precache-manifest.c1fec2680a1d0918b9512eb56ce5ed8b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540af452c92293226566bff378a795c7",
+    "url": "/cryptoCoinsProject/index.html"
+  },
+  {
+    "revision": "ef03a5e3185a6e80844a",
+    "url": "/cryptoCoinsProject/static/css/main.d74c8d86.chunk.css"
+  },
+  {
+    "revision": "cff58483527d6f255ac3",
+    "url": "/cryptoCoinsProject/static/js/2.7829ecf7.chunk.js"
+  },
+  {
+    "revision": "a5baca1c6ad4d7d6812837b45afe3ee2",
+    "url": "/cryptoCoinsProject/static/js/2.7829ecf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef03a5e3185a6e80844a",
+    "url": "/cryptoCoinsProject/static/js/main.7b777d1f.chunk.js"
+  },
+  {
+    "revision": "961cf8db2578dba0ea41",
+    "url": "/cryptoCoinsProject/static/js/runtime-main.315cbe9c.js"
+  },
+  {
+    "revision": "b272803d36f912ab25002e9366d98f43",
+    "url": "/cryptoCoinsProject/static/media/back.b272803d.svg"
+  },
+  {
+    "revision": "aa04453bf43fc7346b823bc1b793b3c7",
+    "url": "/cryptoCoinsProject/static/media/next.aa04453b.svg"
+  },
+  {
+    "revision": "7eb08b71c0dac161fa27b1f4b3f78c42",
+    "url": "/cryptoCoinsProject/static/media/preloader.7eb08b71.svg"
+  },
+  {
+    "revision": "fe4db34f9c00d8930c1142d1dce91515",
+    "url": "/cryptoCoinsProject/static/media/sort-down.fe4db34f.svg"
+  },
+  {
+    "revision": "9aa61a2dfef153cbab742c8b7e9beb48",
+    "url": "/cryptoCoinsProject/static/media/sort-up.9aa61a2d.svg"
+  }
+]);
